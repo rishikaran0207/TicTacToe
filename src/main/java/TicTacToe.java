8dd3b8b6c84e9c253db35cfbbdcf8ce21f374b0d
rishikaran0207 <sendittomemrk@gmail.com>
2021-08-30 09:41:24 +0530

    Added: Createaboard

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,6 +1,19 @@
 package com.Bridgelabz;
+import java.util.Scanner;
 public class TicTacToe {
-    public static void main(String[] args){
-        System.out.println("Welcome to Workshop " );
+    public static void main(String[] args) {
+        System.out.println("Welocme to TicTacToe");
+    }
+
+    public static void boardcreate() {
+        char[][] board = new char[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+
+        }
     }
 }
+
+
