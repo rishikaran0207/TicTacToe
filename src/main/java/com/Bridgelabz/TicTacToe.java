5926efd021f3f018f4be9ed5240a9d2596932de2
rishikaran0207 <sendittomemrk@gmail.com>
2021-09-01 00:12:07 +0530

    Added: Selecting a box

--- src/main/java/com/Bridgelabz/TicTacToe.java
+++ src/main/java/com/Bridgelabz/TicTacToe.java
@@ -2,6 +2,7 @@ package com.Bridgelabz;
 import java.util.Scanner;
 public class TicTacToe {
     public static char board[] = new char[10];
+    public static int box,choice,move,cplay,count,i;
     public static char PChoice,computerchoice;
     public static Scanner sc = new Scanner(System.in);
     public static void boardcreate() {
@@ -37,13 +38,24 @@ public class TicTacToe {
         System.out.println(" ----------- ");
         System.out.println("  " + board[7] + " | " + board[8] + " | " + board[9] + " \n");
     }
-
-
+        public static void selectbox() {
+            System.out.println("Select box you need 1 to 9:");
+            int box = sc.nextInt();
+            if (box > 9 || box < 1) {
+                System.out.println("you chose wrong box!!!");
+                selectbox();
+            } else if ( board[box] != ' ') {
+                System.out.println("The box is Occupied already");
+                selectbox();
+            } else
+                board[box] = PChoice;
+            printBoard();
+        }
     public static void main(String[] args) {
         System.out.println("Welocme to TicTacToe");
         boardcreate();
         options();
         printBoard();
-
+        selectbox();
     }
 }
