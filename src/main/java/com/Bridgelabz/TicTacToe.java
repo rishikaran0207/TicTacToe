5e764c3a7e7ad602064c4acd41e51d143c028d43
rishikaran0207 <sendittomemrk@gmail.com>
2021-08-31 23:58:00 +0530

    Added: Displaying the board

--- src/main/java/com/Bridgelabz/TicTacToe.java
+++ src/main/java/com/Bridgelabz/TicTacToe.java
@@ -1,19 +1,14 @@
 package com.Bridgelabz;
 import java.util.Scanner;
-
 public class TicTacToe {
+    public static char board[] = new char[10];
     public static char PChoice,computerchoice;
     public static Scanner sc = new Scanner(System.in);
-
-
     public static void boardcreate() {
-        char board[] = new char[10];
         for (int i = 1; i < board.length; i++) {
-              board[i] = ' ';
-            }
+            board[i] = ' ';
         }
-
-
+    }
     public static void options() {
         System.out.println("\nEnter the 'X' or 'O' what you needed:");
         PChoice = sc.next().charAt(0);
@@ -34,13 +29,21 @@ public class TicTacToe {
         System.out.println("You chose " + PChoice+" \n");
         System.out.println("Computer is " + computerchoice+" \n");
     }
+    public static void printBoard(){
+        System.out.println();
+        System.out.println("\n " + board[1] + "  | " + board[2] + " | " + board[3] + " ");
+        System.out.println(" ----------- ");
+        System.out.println("  " + board[4] + " | " + board[5] + " | " + board[6] + " ");
+        System.out.println(" ----------- ");
+        System.out.println("  " + board[7] + " | " + board[8] + " | " + board[9] + " \n");
+    }
 
 
     public static void main(String[] args) {
-
         System.out.println("Welocme to TicTacToe");
         boardcreate();
         options();
+        printBoard();
 
     }
 }
